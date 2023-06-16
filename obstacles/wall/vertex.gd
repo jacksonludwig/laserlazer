16d92f3af66a523fbe701386fe8c0c3b1f62834a
Jackson Ludwig <jacksonludwig0@gmail.com>
2023-06-16 08:20:19 -0400

    add placeholder path follow

--- obstacles/wall/vertex.gd
+++ obstacles/wall/vertex.gd
@@ -7,19 +7,22 @@ enum StatusType {
 	ACTIVE
 }
 
+## Modifiers with lower values will take precedence in some cases
+## (e.g. if frozen and sped up, it will remain just frozen)
 enum MovementModifier {
 	NONE = 0,
 	FROZEN = 1,
 	REVERSED = 2,
+	FAST = 4,
 }
 
 ## emitted when the wall state changes (e.g. the wall becomes ACTIVE)
 signal status_change(new_status: StatusType)
 
 @export_category("Movement Options")
-@export_flags("Frozen", "Reverse", "Rotate") var movement_state_modifier = 0
-@export var speed_modifer = 1
-@export var speed = 1
+@export_flags("Frozen", "Reverse", "Rotate", "Fast") var movement_state_modifier: int = 0
+@export var speed_modifer: int = 1
+@export var speed: int = 1
 @export_category("")
 
 @export_category("Status Options")
@@ -32,4 +35,21 @@ func _ready():
 
 
 func _process(_delta):
+	## movement is controlled through the defined path
+	if get_parent().get_class() == "WallFollowPath":
+		pass
+	
+	## movement controlled through code
 	pass
+
+
+## get the vertex's speed after modifiers have been applied
+func _get_modified_speed():
+	if Utils.check_bit_flag(movement_state_modifier, MovementModifier.FROZEN):
+		pass
+	
+	if Utils.check_bit_flag(movement_state_modifier, MovementModifier.FAST):
+		pass
+		
+	if Utils.check_bit_flag(movement_state_modifier, MovementModifier.REVERSED):
+		pass
---
+++ obstacles/wall/wall_follow_path.gd
@@ -0,0 +1,10 @@
+class_name WallFollowPath
+extends PathFollow2D
+## Attach vertices to this path in order to have the wall follow the set path.
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
