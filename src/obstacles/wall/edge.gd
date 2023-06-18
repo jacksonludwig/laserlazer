a4b138d5233e5f42a5b42feea67e8d520cf8f668
Jackson <jacksonludwig0@gmail.com>
2023-06-18 14:34:12 -0400

    add placeholder values for edge

--- src/obstacles/wall/edge.gd
+++ src/obstacles/wall/edge.gd
@@ -2,14 +2,49 @@ class_name Edge
 extends Area2D
 ## Static edge that expects to be a child of a vertex
 
+## this status should be inherited from the vertex and not be modified manually
 @export var status: Enums.StatusType = Enums.StatusType.INACTIVE
 
+@onready var vertex: Vertex = get_parent()
+@onready var collision_shape: CollisionShape2D = get_child(0)
+
+## the collision and lazer will extend to these values when the edge is active
+var max_collision_width: float
+var max_collision_length: float
 
 func _ready():
-	var vertex := get_parent()
+	assert(collision_shape is CollisionShape2D)
+	assert(collision_shape.shape is RectangleShape2D, "expected edge's collision shape to have a rectangular bounding box")
 	assert(vertex is Vertex, "expected parent of edge to always be vertex")
+	
+	# save the maximum dimensions of the collision shape
+	max_collision_length = collision_shape.shape.size.x
+	max_collision_width = collision_shape.shape.size.y
+	
+	# set the collision box to zero size if the default state is inactive
+	if status == Enums.StatusType.INACTIVE:
+		collision_shape.shape.size = Vector2.ZERO
+	
 	vertex.connect("status_change", _on_active_status_changed)
 
 
 func _on_active_status_changed(new_status: Enums.StatusType):
+	if status == new_status: return
+	
 	status = new_status
+	
+	match new_status:
+		Enums.StatusType.ACTIVE:
+			_on_become_active()
+		Enums.StatusType.INACTIVE:
+			_on_become_inactive()
+
+
+## tween the collision and laser to maximum size
+func _on_become_active():
+	pass
+	
+
+## tween the collision and laster to minimum size
+func _on_become_inactive():
+	pass
