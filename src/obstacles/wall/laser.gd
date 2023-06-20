6ddae83b61a70fca78c22ccb1f793265a3142cee
Jackson Ludwig <jacksonludwig0@gmail.com>
2023-06-20 19:06:22 -0400

    dont use shapecast

--- src/obstacles/wall/laser.gd
+++ src/obstacles/wall/laser.gd
@@ -1,29 +1,33 @@
 class_name Laser
-extends ShapeCast2D
+extends Line2D
 ## Laser wall
 
 ## this status should be inherited from the vertex and not be modified manually
 @export var status: Enums.StatusType = Enums.StatusType.INACTIVE
-
-## the collision and laser will extend to these values when the edge is active.
-@onready var end_point := Vector2(self.target_position.x, self.target_position.y)
-@onready var laser_width: float = self.shape.size[1]
-
-@onready var vertex: Vertex = get_parent()
-
 ## number of seconds it takes to animate between statuses
-var active_status_change_duration: float = 3
+@export var active_status_change_duration: float = 3
+
+## the collision and laser will extend to this point when active
+@onready var end_point := Vector2(self.points[1].x, self.points[1].y)
+@onready var start_point := Vector2(self.points[0].x, self.points[0].y)
+
+@onready var collision_shape: CollisionShape2D = $Area2D.get_child(0)
+@onready var parent_vertex: Vertex = get_parent()
 
 ## tween used for animating state changes
 var _tween: Tween
 
+## TODO: extend collision shape to match laser
 
 func _ready():
+	# ignore any extra points added in editor
+	self.points = self.points.slice(0, 2)
+	
 	# set the laser to zero size if the default state is inactive
 	if status == Enums.StatusType.INACTIVE:
-		_update_laser_ending_point(Vector2.ZERO)
+		_update_laser_ending_point(start_point)
 
-	vertex.connect("status_change", _on_active_status_changed)
+	parent_vertex.connect("status_change", _on_active_status_changed)
 
 
 func _on_active_status_changed(new_status: Enums.StatusType):
@@ -39,21 +43,20 @@ func _on_active_status_changed(new_status: Enums.StatusType):
 ## tween the collision and laser to maximum size
 func _on_become_active():
 	_get_tween().tween_method(
-		_update_laser_ending_point, self.target_position, end_point, active_status_change_duration
+		_update_laser_ending_point, self.points[1], end_point, active_status_change_duration
 	)
 
 
 ## tween the collision and laser to minimum size
 func _on_become_inactive():
 	_get_tween().tween_method(
-		_update_laser_ending_point, self.target_position, Vector2.ZERO, active_status_change_duration
+		_update_laser_ending_point, self.points[1], start_point, active_status_change_duration
 	)
 
 
 ## update the collision and visual line of the laser to end at the given point
 func _update_laser_ending_point(end_point: Vector2):
-	self.target_position = end_point
-	$LaserVisual.points[1] = end_point
+	self.points[1] = end_point
 
 
 func _get_tween():
