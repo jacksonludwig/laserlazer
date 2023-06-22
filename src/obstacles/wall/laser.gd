55448616d46eecc19bcd192839693d185c207d47
Jackson Ludwig <jacksonludwig0@gmail.com>
2023-06-21 22:22:38 -0400

    don't use scale for collision shapes apparently

--- src/obstacles/wall/laser.gd
+++ src/obstacles/wall/laser.gd
@@ -17,12 +17,11 @@ extends Line2D
 ## tween used for animating state changes
 var _tween: Tween
 
-## TODO: extend collision shape to match laser
 
 func _ready():
 	# ignore any extra points added in editor
 	self.points = self.points.slice(0, 2)
-	
+
 	# set the laser to zero size if the default state is inactive
 	if status == Enums.StatusType.INACTIVE:
 		_update_laser_ending_point(start_point)
@@ -42,6 +41,10 @@ func _on_active_status_changed(new_status: Enums.StatusType):
 
 ## tween the collision and laser to maximum size
 func _on_become_active():
+	## if needed, re-active collision immediately
+	if collision_shape.disabled:
+		collision_shape.disabled = false
+
 	_get_tween().tween_method(
 		_update_laser_ending_point, self.points[1], end_point, active_status_change_duration
 	)
@@ -55,11 +58,21 @@ func _on_become_inactive():
 
 
 ## update the collision and visual line of the laser to end at the given point
-func _update_laser_ending_point(end_point: Vector2):
-	self.points[1] = end_point
+func _update_laser_ending_point(new_end_point: Vector2):
+	self.points[1] = new_end_point
+	collision_shape.position = new_end_point / 2
+	collision_shape.shape.size = Vector2(new_end_point[0], self.width)
+
+
+func _on_tween_finished():
+	## if the status is inactive and the tween just finished, disable collision
+	if status == Enums.StatusType.INACTIVE:
+		collision_shape.disabled = true
 
 
 func _get_tween():
 	if _tween:
-		return _tween
-	return create_tween()
+		_tween.kill()
+	_tween = create_tween()
+	_tween.connect("finished", _on_tween_finished)
+	return _tween
